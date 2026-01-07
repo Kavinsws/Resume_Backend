94c72d1a7c1aac8d86b7d5190699cdbc4b118390
Kavin778 <kavin.m6578@gmail.com>
2026-01-07 17:37:52 +0530

    Error Handling

---
+++ Backend/src/middleware/Validation.ts
@@ -0,0 +1,17 @@
+import { ZodObject } from "zod";
+import { Request, Response, NextFunction } from "express";
+
+export const validate =
+  (schema: ZodObject<any>) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    try{
+        req.body = schema.parse(req.body);
+        next();
+    }
+    catch(error:any){
+        return res.status(400).json({
+            message:"Validation failed",
+            error: error.issues
+        })
+    }
+  };
