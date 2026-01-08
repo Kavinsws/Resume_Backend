c91610c5a836f41654da6780009b6f4069befb69
Kavin778 <kavin.m6578@gmail.com>
2026-01-08 10:50:26 +0530

    Added structured folder for openapi and added error handling in repo

--- Backend/src/repository/JobRepo.ts
+++ Backend/src/repository/JobRepo.ts
@@ -1,10 +1,21 @@
+import { HttpError } from "../error/HttpError";
 import { JobModel } from "../model/Job";
 import { createJob } from "../validator/JobSchemaValidator";
 
 export const createJobRepo = async (data:createJob)=>{
-    return JobModel.create(data)
+    try{
+        return JobModel.create(data);
+    }
+    catch(error){
+        throw new HttpError(500,"Failed to create job");
+    }
 }
 
 export const findJobByTitle = async (title : string)=>{
-    return JobModel.findOne({title})
+    try{
+        return JobModel.findOne({ title });
+    }
+    catch(error){
+        throw new HttpError(500,"Internal server Error");
+    }
 }
